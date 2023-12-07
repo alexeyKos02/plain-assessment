bbc66ef11d638f5e984a4d193dd0b63a1b5f20e8
korol <akorolev@edu.hse.ru>
2023-12-07 13:27:56 +0300

    изменена функция fetchQuestions, изменен порядок функций и другие мелкие исправления

--- quiz-app/scripts/quiz.js
+++ quiz-app/scripts/quiz.js
@@ -1,9 +1,64 @@
 import {fetchQuestions} from "./fetchQuestions.js";
 
-const questions = await fetchQuestions()
+const questions = []
 const correctQuestions = []
-const form = document.getElementById("quiz-wrapper").children[0]
-changeDomElements()
+const form = document.forms.quizForm
+
+function changeModalCounter() {
+    const modalCounter = document.getElementById("my-modal")
+    const span = modalCounter.getElementsByTagName("span")[0]
+    let number = correctQuestions.filter(question => question).length
+    span.innerText = `${number}/5`
+}
+
+function changeCounter() {
+    const counter = document.getElementById("aside-counter")
+    const span = counter.getElementsByTagName("span")[0]
+    let number = correctQuestions.filter(question => question).length
+    span.innerText = `${number}/5`
+}
+
+function changeNumberCorrectAnswers() {
+    changeCounter()
+    changeModalCounter()
+}
+
+function paintAnswers(answerId, correctId, section) {
+    const questionId = [...section.parentNode.children].findIndex((elem)=> elem === section)
+    section.children[correctId].style.backgroundColor = "green"
+    if (answerId !== correctId) {
+        section.children[answerId].style.backgroundColor = "red"
+        correctQuestions[questionId] = false
+    } else {
+        correctQuestions[questionId] = true
+    }
+    changeNumberCorrectAnswers()
+}
+
+function clearBackgroundAllAnswers(section) {
+    const answersDivs = section.getElementsByTagName("div")
+    for (let div of answersDivs) {
+        div.style.backgroundColor = "transparent"
+    }
+}
+
+function checkAnswer(e, questionId) {
+    const section = e.target.parentNode.parentNode.parentNode
+    clearBackgroundAllAnswers(section)
+    const answerId = Number(e.target.value)
+    const correctId = questions.find(question => question.id === questionId + 1).answer
+    paintAnswers(answerId, correctId, section)
+}
+
+function addModalActions() {
+    document.querySelector("#my-modal .modal__box").addEventListener('click', event => {
+        event._isClickWithInModal = true;
+    });
+    document.getElementById("my-modal").addEventListener('click', event => {
+        if (event._isClickWithInModal) return;
+        event.currentTarget.classList.remove('open');
+    });
+}
 
 function changeDomElements() {
     let sectionIdx = 0
@@ -27,58 +82,15 @@ function changeDomElements() {
     addModalActions()
 }
 
-function addModalActions() {
-    document.querySelector("#my-modal .modal__box").addEventListener('click', event => {
-        event._isClickWithInModal = true;
-    });
-    document.getElementById("my-modal").addEventListener('click', event => {
-        if (event._isClickWithInModal) return;
-        event.currentTarget.classList.remove('open');
-    });
-}
-
-function checkAnswer(e, questionId) {
-    const section = e.target.parentNode.parentNode.parentNode
-    clearBackgroundAllAnswers(section)
-    const answerId = Number(e.target.value)
-    const correctId = questions.find(question => question.id === questionId + 1).answer
-    paintAnswers(answerId, correctId, section)
-}
-
-function clearBackgroundAllAnswers(section) {
-    const answersDivs = section.getElementsByTagName("div")
-    for (let div of answersDivs) {
-        div.style.backgroundColor = "transparent"
-    }
-}
-
-function paintAnswers(answerId, correctId, section) {
-    const questionId = [...section.parentNode.children].indexOf(section)
-    section.children[correctId].style.backgroundColor = "green"
-    if (answerId !== correctId) {
-        section.children[answerId].style.backgroundColor = "red"
-        correctQuestions[questionId] = false
+fetchQuestions().then((response) => {
+    if (response.ok) {
+        response.json().then(data => {
+            for (let question of data) {
+                questions.push({id: question.id, answer: question.answer})
+            }
+        })
     } else {
-        correctQuestions[questionId] = true
+        alert("Ошибка HTTP: " + response.status);
     }
-    changeNumberCorrectAnswers()
-}
-
-function changeNumberCorrectAnswers() {
-    changeCounter()
-    changeModalCounter()
-}
-
-function changeModalCounter() {
-    const modalCounter = document.getElementById("my-modal")
-    const span = modalCounter.getElementsByTagName("span")[0]
-    let number = correctQuestions.filter(question => question).length
-    span.innerText = `${number}/5`
-}
-
-function changeCounter() {
-    const counter = document.getElementById("aside-counter")
-    const span = counter.getElementsByTagName("span")[0]
-    let number = correctQuestions.filter(question => question).length
-    span.innerText = `${number}/5`
-}
+    changeDomElements()
+})
